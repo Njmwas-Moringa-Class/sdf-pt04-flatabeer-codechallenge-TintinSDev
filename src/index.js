717a91ca3998061dee21f2f925150f3268d7d9eb
Martin <mmaina290@gmail.com>
2023-10-25 20:46:50 +0300

    fetch beers

--- src/index.js
+++ src/index.js
@@ -7,73 +7,21 @@ async function fetchBeers() {
     const beers = await beersResponse.json()
     return fetch(`${BASE_URL}/beers`)
 }
-
+const beerReviews = document.querySelector('#review-list')
+const beerName = document.querySelector('#beer-name')
+const beerImage = document.querySelector('#beer-image')
+const beerDescription = document.querySelector('#beer-description')
+const beerList = document.querySelector('#beer-list')
+const beerDetails = document.querySelector('.beer-details')
+const description = document.querySelector('#description-form')
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    // document.getElementsByClassName("beer-details")
-    // document.addEventListener('submit', (event) => {
-    //     event.preventDefault()
-    //     console.log(event)
-    //     const formData = new FormData(event.target)
-    //     const description = formData.get('description')
-    //     fetch(`${BASE_URL}/beers`, {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify({ description })
-    //     })
-    //         .then(response => response.json())
-    //         .then(beer => {
-    //             console.log(beer)
-    //         })
-    // })
-    
-  
-    
 
-})
-
-// const beerDetails = document.querySelector('.beer-details')
-
-// beerDetails.addEventListener('click', beerListClickHandler)
-// function beerListClickHandler(beerList) {
-//     fetch(`${BASE_URL}/beers/${beerList.target.id}`, {
-//         method: 'PATCH',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(beerList)
-//     })
-//         .then(response => response.json())
-//         .then(beer => {
-//             console.log(beer)
-//         })
+    beerDetails.addEventListener('click', (e) => {
+        e.preventDefault();
         
-  
-
-// }
-const beerList = document.querySelector('#beer-list')
-    beerList.addEventListener('click', (e) => {
-        e.preventDefault()
-        console.log(e)
-        const formData = new FormData(e.target)
-        const description = formData.get('description')
-        fetch(`${BASE_URL}/beers`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ description })
-        })
-            .then(response => response.json())
-            .then(beer => {
-                console.log(beer)
-            })
     })
-    
-     
     fetch(`${BASE_URL}/beers`)
         .then(response => response.json())
         .then(beers => {
@@ -81,62 +29,15 @@ const beerList = document.querySelector('#beer-list')
             for (let beer of beers) {
                 beerList.innerHTML += `<li>${beer.name}</li>`
             }
-            
-        })
-    
-    //beer name display
-    const beerName = document.querySelector('#beer-name')
-    fetch(`${BASE_URL}/beers`)
-        .then(response => response.json())
-        .then(beers => {
             beerName.innerText = beers[0].name
-        })
-    
-    //beer-image dispaly
-    const beerImage = document.querySelector('#beer-image')
-
-    fetch(`${BASE_URL}/beers`)
-        .then(response => response.json())
-        .then(beers => {
             beerImage.src = beers[0].image_url
-
-        })
-
-    // beer-description
-    const beerDescription = document.querySelector('#beer-description')
-
-    fetch(`${BASE_URL}/beers`)
-        .then(response => response.json())
-        .then(beers => {
             beerDescription.innerHTML = beers[0].description
             Map.description = Object.keys(beerDescription)
-        })
-    
-    //beer description
-    const beerReviews = document.querySelector('#review-list')
-    fetch(`${BASE_URL}/beers`)
-        .then(response => response.json())
-        .then(beers => {
             beerReviews.innerText = beers[0].reviews
             Map.reviews = Object.keys(beerReviews)
-
         })
+    
+        
+    })
+       
 
-
-
-  
-
-    // const form = document.getElementById('description-form')
-    // form.addEventListener('submit', (e) => {
-    //     e.preventDefault()
-    //     const formData = ''
-    //     const description = formData.get('description')
-    //     fetch(`${BASE_URL}/beers`, {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify({ description })
-
-    //     })
-    // })
