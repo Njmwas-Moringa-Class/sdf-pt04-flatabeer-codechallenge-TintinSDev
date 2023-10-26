2c33ea36d6352af12e7dd5e9d6dd660fc569a19b
Martin <mmaina290@gmail.com>
2023-10-26 06:50:49 +0300

    nav bar event listener succesful

--- src/index.js
+++ src/index.js
@@ -12,64 +12,54 @@ async function fetchBeers() {
 
 document.addEventListener('DOMContentLoaded', () => {
     const beerReviews = document.querySelector('#review-list')
-const beerName = document.querySelector('#beer-name')
-const beerImage = document.querySelector('#beer-image')
-const beerDescription = document.querySelector('#beer-description')
-const beerList = document.querySelector('#beer-list')
-const beerDetails = document.querySelector('.beer-details')
-const description = document.querySelector('#description-form')
+    const beerName = document.querySelector('#beer-name')
+    const beerImage = document.querySelector('#beer-image')
+    const beerDescription = document.querySelector('#beer-description')
+    const beerList = document.querySelector('#beer-list')
+    const beerDetails = document.querySelector('.beer-details')
+    const description = document.querySelector('#description-form')
 
     fetch(`${BASE_URL}/beers`)
-    .then(response => response.json())
-    .then(beers => {
-        beerList.innerHTML = ''
-        for (let beer of beers) {
-            beerList.innerHTML += `<li>${beer.name}</li>`
-        }
-        beerName.innerText = beers[0].name
-        beerImage.src = beers[0].image_url
-        beerDescription.innerHTML = beers[0].description
-        beerReviews.innerText = beers[0].reviews
-    })
+        .then(response => response.json())
+        .then(beers => {
+            beerList.innerHTML = ''
+            for (let beer of beers) {
+                beerList.innerHTML += `<li>${beer.name}</li>`
+            }
+            beerName.innerText = beers[0].name
+            beerImage.src = beers[0].image_url
+            beerDescription.innerHTML = beers[0].description
+            beerReviews.innerText = beers[0].reviews
+        })
 
     description.addEventListener('submit', (e) => {
         e.preventDefault();
         function updateBeers(beers) {
-            fetch(`${BASE_URL}/beers`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(beers)
-            })
+            fetch(`${BASE_URL}/beers`,)
+                
                 .then(res => res.json())
                 .then(beer => console.log(beer))
+        
+            updateBeers();
         }
-        updateBeers();
-            
-    })
-
-    beerList.addEventListener('click', (e) => {
-        e.preventDefault();
-         //console.log(e.target.innerText);
-        fetch(`${BASE_URL}/beers`,)
-            .then(response => response.json())
-            .then(beers => {
-                for (let beer of beers) {
-                    if (beer.name === e.target.textContent) {
-                        beerName.innerText = beer.name
-                        beerImage.src = beer.image_url
-                        beerDescription.innerHTML = beer.description
-                        beerReviews.innerHTML = beer.reviews
+        //nav bar beers navigation #successfull
+        beerList.addEventListener('click', (e) => {
+            e.preventDefault();
+            //console.log(e.target.innerText);
+            fetch(`${BASE_URL}/beers`,)
+                .then(response => response.json())
+                .then(beers => {
+                    for (let beer of beers) {
+                        if (beer.name === e.target.textContent) {
+                            beerName.innerText = beer.name
+                            beerImage.src = beer.image_url
+                            beerDescription.innerHTML = beer.description
+                            beerReviews.innerHTML = beer.reviews
+                        }
                     }
-                }
-            })
-        
-        
+                })
+        })
     })
-   
-        
-        
-    })
+})
        
 
